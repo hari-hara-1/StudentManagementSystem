4c35a80830ecec633ac1d3ef64b94d6bdf81c521
hari-hara-1 <hari.shenbagan@gmail.com>
2025-12-09 21:43:05 +0530

    Security Update: Modified the Database Credentials storage mechanism..

--- src/main/java/com/hari/studentmgtsystem/StudentManagementSystemApplication.java
+++ src/main/java/com/hari/studentmgtsystem/StudentManagementSystemApplication.java
@@ -3,10 +3,21 @@ package com.hari.studentmgtsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class StudentManagementSystemApplication {
 
     public static void main(String[] args) {
+        
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
         SpringApplication.run(StudentManagementSystemApplication.class, args);
     }
 
